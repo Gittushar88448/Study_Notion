00de41cab9f70aef92470a9f78f42596634dbcae
Tushar <kmrtushar01@gmail.com>
2024-08-03 11:31:54 +0530

    having data about user can rate and give reviews

---
+++ modal/RatingAndReviewSchema.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const ratingAndReviewSchema = new mongoose.Schema({
+
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    rating: {
+        type: Number,
+    },
+    review: {
+        type: String,
+        trim: true
+    }
+})
+
+module.exports = mongoose.model("RatingAndReview", ratingAndReviewSchema);
