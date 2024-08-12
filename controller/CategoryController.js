893185c2f1617406c0d816394df4b4cc2f1e9186
Tushar <kmrtushar01@gmail.com>
2024-08-12 19:01:52 +0530

    fetching category page details having selected category, different category and having top most saled course details

--- controller/CategoryController.js
+++ controller/CategoryController.js
@@ -59,3 +59,67 @@ exports.getAllCategory = async (req, res) => {
     }
 }
 
+// getting category page details
+
+exports.getCategoryPageDetails = async(req, res) =>{
+    try{
+        const {categoryId} = req.body;
+
+        if(!categoryId){
+            return res.status(404).json({
+                success: false,
+                message: "Failed to fetch category id",
+            });
+        }
+
+        const selectedCategory = await Category.findOne({_id: categoryId}).populate("course").exec();
+
+        if(!selectedCategory){
+            return res.status(404).json({
+                success: false,
+                message: "Course details not found"
+            })
+        }
+
+        //Handling the case when no course found for that particular id
+        if(selectedCategory.course.length === 0){
+            console.log("No course found for particular id");
+            return res.status(404).json({
+                success: false,
+                message: "Courses not available for this category"
+            })
+        }
+
+        const selectedCourses = selectedCategory.course;
+
+        // get courses from other category
+
+        const categoryExceptSelectedCategory = await Category.find({_id: {$ne: categoryId}}).populate("course").exec();
+
+        const differentCourse = [];
+
+        for(const category of categoryExceptSelectedCategory){
+            differentCourse.push(category.course);
+        }
+
+        // finding top 10 most selling courses
+        const topCategory = await Category.find().populate("course").exec();
+        const topCourse = topCategory.flatMap((category) => category.course);
+        const topMostSellingCourse = topCourse.sort((a,b) => b.sold - a.sold).slice(0, 10);
+
+        return res.status(200).json({
+            success: true,
+            selectedCourses: selectedCourses,
+            differentCourse: differentCourse,
+            topMostSellingCourse: topMostSellingCourse,
+        })
+
+    }catch(error){
+        console.error(error);
+        res.status(500).json({
+            success: false,
+            message: "Something went wrong while getting Category page details",
+            error: error.message,
+        });
+    }
+}
