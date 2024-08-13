e58f413d6a2522e4a77fbebb25020ee5e417a164
Tushar <kmrtushar01@gmail.com>
2024-08-13 11:18:23 +0530

    controller of creating course

---
+++ controller/CourseController.js
@@ -0,0 +1,104 @@
+
+const Category = require('../modal/CategorySchema');
+const User = require('../modal/UserSchema');
+const { imageUploadToCloudinary } = require('../util/imageUploader');
+require('dotenv').config();
+const Course = require('../modal/Course');
+
+// Course creation  
+exports.createCourse = async (req, res) => {
+
+    try {
+        // fetching details from request's body
+        const { courseName, courseDescription, whatYouWillLearn, price, category, tag } = req.body;
+
+        // fetching file thumbnail image
+        const thumbnail = req.files.thumbnailImage;
+
+        console.log("thumbnail ",thumbnail);
+
+        // Validation
+        if (!courseName || !courseDescription || !whatYouWillLearn || !price || !tag || !category) {
+            return res.status(400).json({
+                success: false,
+                message: "Please enter all the details carefully",
+            });
+        }
+
+        // fetching user id from user token
+        const userId = req.user.id;
+        
+        // Instructor Validation
+        if (!userId) {
+            return res.status(404).json({
+                success: false,
+                message: "User id not found"
+            })
+        }
+
+        // fetching instructor details
+        const instructorDetails = await User.findById(userId);
+        console.log("Instructor details", instructorDetails);
+
+
+        // validate
+        if (!instructorDetails) {
+            return res.status(404).json({
+                success: false,
+                message: "Instructor details not found in database"
+            });
+        }
+
+        const categoryDetails = await Category.findById(category);
+
+        if (!categoryDetails) {
+            return res.status(404).json({
+                success: false,
+                message: "category details not found in the collection"
+            });
+        }
+        console.log("category details", categoryDetails);
+
+        const thumbnailUpload = await imageUploadToCloudinary(thumbnail, process.env.FOLDER_NAME);
+        console.log("Thumbnail upload",thumbnailUpload);
+
+        const newCourse = await Course.create({
+            courseName,
+            courseDescription,
+            instructor: instructorDetails._id,
+            whatYouWillLearn: whatYouWillLearn,
+            price,
+            tag: tag,
+            category: categoryDetails._id,
+            thumbnail: thumbnailUpload.secure_url,
+        });
+
+        await User.findByIdAndUpdate({ _id: userId }, {
+            $push: {
+                courses: newCourse._id
+            }
+        }, { new: true });
+
+        await Category.findByIdAndUpdate(
+            { _id: category },
+            {$push:{course: newCourse._id,}},
+            { new: true }
+        )
+
+        res.status(200).json({
+            success: true,
+            message: "Course created successfully",
+            newCourse
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            success: false,
+            message: "Something went wrong while creating course"
+        })
+    }
+
+
+}
+
+
